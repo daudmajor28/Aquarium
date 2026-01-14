74828ebe4c72a4bf87a09f8e0fa9f2ed9aedda9c
daudmajor <daud_major28@milton.edu>
2026-01-14 09:03:41 -0500

    Merge remote-tracking branch 'origin/main'

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -38,13 +38,13 @@ public class BasicGameApp implements Runnable {
    public JPanel panel;
    
 	public BufferStrategy bufferStrategy;
-	public Image astroPic;
-    public Image AstroidPic;
+	public Image Bob;
+    public Image Futbol;
     public Image backgroundPic;
 
    //Declare the objects used in the program
    //These are things that are made up of more than one variable type
-	private Astronaut astro;
+	public Astronaut astro;
     public Astronaut astro2;
     public Astroid  astroid2;
     public Astroid  astroid1;
@@ -79,20 +79,21 @@ public class BasicGameApp implements Runnable {
        
       //variable and objects
       //create (construct) the objects needed for the game and load up 
-		astroPic = Toolkit.getDefaultToolkit().getImage("astronaut.png"); //load the picture
-        AstroidPic = Toolkit.getDefaultToolkit().getImage("Astoid.jpg"); //load the picture
-        backgroundPic = Toolkit.getDefaultToolkit().getImage("stars.jpg"); //load the picture
+		Bob = Toolkit.getDefaultToolkit().getImage("Runner.png"); //load the picture
+        Futbol = Toolkit.getDefaultToolkit().getImage("Subject.png"); //load the picture
+        backgroundPic = Toolkit.getDefaultToolkit().getImage("pitch.jpg"); //load the picture
 
-        astro = new Astronaut(20,300);
-        astro.dy=2;
-        astro.height=40;
-        astro.width=40;
+        astro = new Astronaut(randy,randx);
+        astro.dy=-2;
+        astro.height=1;
+        astro.width=1;
+
         astro2 = new Astronaut(randy,randx);
         astro2.dy=-2;
-        astro2.height=50;
-        astro2.width=50;
+        astro2.height=150;
+        astro2.width=150;
 
-        astroid1 = new Astroid(100,420);
+        astroid1 = new Astroid(100,800);
         astroid1.dx= -astroid1.dx;
         astroid2 = new Astroid(randy,467);
 	}// BasicGameApp()
@@ -196,11 +197,11 @@ crashing();
         g.drawImage(backgroundPic, 0, 0, WIDTH, HEIGHT, null);
       //draw the image of the astronaut
 
-		g.drawImage(astroPic, astro.xpos, astro.ypos, astro.width, astro.height, null);
+		g.drawImage(Bob, astro.xpos, astro.ypos, astro.width, astro.height, null);
         if(astro2.isAlive == true){
-		g.drawImage(astroPic, astro2.xpos, astro2.ypos, astro2.width, astro2.height, null);}
-        g.drawImage(AstroidPic, astroid1.xpos, astroid1.ypos,astroid1.width,astroid1.height, null);
-        g.drawImage(AstroidPic, astroid2.xpos, astroid2.ypos,astroid2.width,astroid2.height, null);
+		g.drawImage(Bob, astro2.xpos, astro2.ypos, astro2.width, astro2.height, null);}
+        g.drawImage(Futbol, astroid1.xpos, astroid1.ypos,astroid1.width,astroid1.height, null);
+        g.drawImage(Futbol, astroid2.xpos, astroid2.ypos,astroid2.width,astroid2.height, null);
 
 g.drawRect(astro.hitbox.x, astro.hitbox.y, astro.hitbox.width, astro.hitbox.height);
 
